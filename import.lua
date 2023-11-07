408b05b7c05895e02134bb4ee6e7ce8db7d33317
Arctos2win <116841243+Arctos2win@users.noreply.github.com>
2023-11-07 14:40:19 +0100

    Update to most recent code

--- import.lua
+++ import.lua
@@ -1,108 +1,118 @@
-local map = {}
+local proxy, methods = {}, {}
 
-buildTable = function(t)
-    local res = {} 
-    for key in pairs(t) do 
-        if type(t[key]) == 'table' then
-            res[key] = buildTable(t[key])
-        else 
-            res[key] = t[key] 
-        end 
-    end 
-    return res 
+function methods:Load()
+    local t = {}
+    for k in pairs(self) do
+        local v = self[k]
+        t[k] = type(v) == 'table' and v:Load() or v
+    end
+    return t
 end
 
-unload = function(t)
-    for key in pairs(t) do
-        if type(t[key]) == 'table' then
-            unload(t[key]) 
-        end  
-    end  
-    map[t].data = nil  
-end 
+function methods:Unload()
+    local t = proxy[self].values
+    for k, v in pairs(t) do
+        if type(v) == 'table' then v:Unload() else t[k] = nil end
+    end
+end
 
+function methods:Destroy()
+    for k, v in pairs(proxy[self].values) do
+        if type(v) == 'table' then v:Destroy() end
+    end
+    proxy[self] = nil
+end
+
+function methods:Delete()
+    for k in pairs(self) do
+        self[k] = nil
+    end
+end
 
 local mt = {
-    	__call = function(t, data)
-     	     if data.action == 'build' then -- this function is for getting the whole cached structured table 
-            	return buildTable(t)
-	     elseif data.action == 'unload' then -- this function is for unloading the cached data 
-	        unload(t)
-		 elseif data.action == '__newindex' then 
-			map[t].__newindex = data.func  
-             end 
-    	end, 
-	
-	__pairs = function(t)
-		return pairs(map[t].keys)
-	end,
-	__index = function(t, k)
-		if not map[t].data[k] and map[t].keys[k] then
-			local value = GetResourceKvpString(map[t].path .. k)
-			map[t].data[k] = tonumber(value) or value
-		end
-		return map[t].data[k]
-	end,
-	__newindex = function(t, k, v)
-		if t[k] == v then return end 
-		
-		if map[t].__newindex then 
-            		map[t].__newindex(t, k, v) 
-        	end
-		
-		if not v then
-			if map[t].keys[k] then
-				map[t].keys[k] = nil
-				DeleteResourceKvp(map[t].path .. k)
-				local value = map[t].data[k]
-				if value then
-					if type(value) == 'table' then
-						for key in pairs(value) do
-							t[k][key] = nil
-						end
-					end
-					map[t].data[k] = nil
-				end
-			end
-		else
-			t[k] = nil
-			if type(v) == 'table' then
-				map[t].data[k] = CreateKVP(map[t].path .. k .. ':')
-				for i in pairs(v) do
-					t[k][i] = v[i]
-				end
-			else
-				SetResourceKvp(map[t].path .. k, tostring(v))
-				map[t].data[k] = v
-			end
-			map[t].keys[k] = true
-		end
-	end
+    __len = function(t)
+        return proxy[t].length
+    end,
+    __pairs = function(t)
+        return pairs(proxy[t].keys)
+    end,
+    __index = function(t, k)
+        local obj = proxy[t]
+        if not obj.values[k] and obj.keys[k] then
+            local v = GetResourceKvpString(obj.path .. k)
+            obj.values[k] = tonumber(v) or v
+        end
+        return obj.values[k] or methods[k]
+    end,
+    __newindex = function(t, k, v)
+        local obj = proxy[t]
+        if v then
+            t[k] = nil
+            if type(v) == 'table' then
+                obj.values[k] = CreateKvp(obj.path .. k .. ':')
+                for key in pairs(v) do
+                    t[k][key] = v[key]
+                end
+            else
+                SetResourceKvp(obj.path .. k, tostring(v))
+                obj.values[k] = v
+            end
+            obj.keys[k] = true
+            if type(k) == 'number' and k > obj.length then obj.length = k end
+        elseif obj.keys[k] then
+            obj.keys[k] = nil
+            DeleteResourceKvp(obj.path .. k)
+            if k == obj.length then
+                local length = 0
+                for key in pairs(t) do
+                    if type(key) == 'number' and key > length then
+                        length = key
+                    end
+                end
+                obj.length = length
+            end
+            local value = obj.values[k]
+            if value then
+                if type(value) == 'table' then
+                    for key in pairs(value) do
+                        t[k][key] = nil
+                    end
+                    proxy[value] = nil
+                end
+                obj.values[k] = nil
+            end
+        end
+    end
 }
 
-function CreateKVP(key)
-	local obj = {}
-	map[obj] = {
-		data = {},
-		keys = {},
-		path = key
-	}
-	setmetatable(obj, mt)
-	return obj
+function CreateKvp(path)
+    local obj = {}
+    proxy[obj] = {path = path, length = 0, keys = {}, values = {}}
+    return setmetatable(obj, mt)
 end
 
 function KVP(key)
-	key = key .. ':'
-	local obj, handler = CreateKVP(key), StartFindKvp(key)
-	for path in function() return FindKvp(handler) end do
-		local last, data = path:gmatch'[^:]*$'(), obj
-		for node in path:sub(#key, -#last - 1):gmatch'[^:]+' do
-			node = tonumber(node) or node
-			if not data[node] then data[node] = {} end
-			data = data[node]
-		end
-		map[data].keys[tonumber(last) or last] = true
-	end
-	EndFindKvp(handler)
-	return obj
+    key = key .. ':'
+    for obj, data in pairs(proxy) do
+        if data.path == key then
+            return obj
+        end
+    end
+    local obj = CreateKvp(key)
+    local handle = StartFindKvp(key)
+    if handle ~= -1 then
+        local path repeat path = FindKvp(handle) if path then
+        local last, data = path:gmatch'[^:]*$'(), obj
+        for node in path:sub(#key, -#last - 1):gmatch'[^:]+' do
+            node = tonumber(node) or node
+            if not data[node] then data[node] = {} end
+            data = data[node]
+        end
+        last = tonumber(last) or last
+        proxy[data].keys[last] = true
+        if type(last) == 'number' and last > proxy[data].length then proxy[data].length = last end
+    end until not path
+        EndFindKvp(handle)
+    end
+    return obj
 end
